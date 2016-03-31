dd2797d8700db81c36ef174fa93660cfbddcfd53
Christopher Henkel <c.henkel@online.de>
2016-03-31 23:05:51 +0200

    init dev branch

--- common/src/main/java/smartwatch/context/common/helper/BleHelper.java
+++ common/src/main/java/smartwatch/context/common/helper/BleHelper.java
@@ -21,12 +21,11 @@ public class BleHelper {
             return -1.0; // if we cannot determine distance, return -1.
         }
 
-        double ratio = rssi*1.0/txPower;
+        double ratio = rssi * 1.0 / txPower;
         if (ratio < 1.0) {
-            return Math.pow(ratio,10);
-        }
-        else {
-            double accuracy =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;
+            return Math.pow(ratio, 10);
+        } else {
+            double accuracy = (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
             return accuracy;
         }
     }
