6909a0c99cecbcff3067c7054e7f788021fd7d59
jtchosen <janthechosen@yahoo.de>
2016-04-09 14:32:55 +0200

    Restrukturierung der Bluetooth Messung

--- common/src/main/java/smartwatch/context/common/helper/BluetoothData.java
+++ common/src/main/java/smartwatch/context/common/helper/BluetoothData.java
@@ -1,5 +1,22 @@
 package smartwatch.context.common.helper;
 
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -10,26 +27,106 @@ import java.util.Queue;
 /**
  * Created by jan on 07.04.16.
  */
-public class BluetoothData {
+public class BluetoothData extends Service implements BeaconConsumer{
 
-    private Queue<Integer> rssiQueueBlue = new LinkedList<>();
-    private Queue<Integer> rssiQueueYellow = new LinkedList<>();
-    private Queue<Integer> rssiQueueRed = new LinkedList<>();
+    private static final String TAG = "Bluetooth Data";
 
-    private final String uuidBlue = "CE:BA:BE:97:DB:0C";
-    private final String uuidYellow = "FB:39:E6:2D:82:EF";
-    private final String uuidRed = "DD:3F:50:F2:76:74";
+    private BeaconManager beaconManager;
 
+    //*^+ Konstanten zur Berechnung der Distanz
+    /*private final double constMult = 0.0001060777;
+    private final double constPower = 17.4228892910;
+    private final double constPlus = 0.7610257596;*/
+
+    private final String[] bluePlaces = {"1", "2", "3"};
+    private final String[] yellowPlaces = {"11", "12", "13"};
+    private final String[] redPlaces = {"21", "22", "23"};
+
+    /*Constant txPower transmitted by the beacons*/
+    private int txPowerBlue;
+    private int txPowerYellow;
+    private int txPowerRed;
+
+    /*Average RSSI of each queue*/
     private double avgBlue = 0;
     private double avgYellow = 0;
     private double avgRed = 0;
+    Map<String, Number> avgRssi;
 
+    /*Queue that holds the measured RSSI for each beacon*/
+    private final Queue<Integer> rssiQueueBlue = new LinkedList<>();
+    private final Queue<Integer> rssiQueueYellow = new LinkedList<>();
+    private final Queue<Integer> rssiQueueRed = new LinkedList<>();
+
+    /*Sets the size of each queue. Better averages vs shorter response to change*/
     private final int queueSize = 5;
 
-    public BluetoothData(Queue<Integer> blue, Queue<Integer> yellow, Queue<Integer> red){
-        rssiQueueBlue = blue;
-        rssiQueueYellow = yellow;
-        rssiQueueRed = red;
+    /*The UUID of each Beacon*/
+    private final String uuidYellow = "FB:39:E6:2D:82:EF";
+    private final String uuidBlue = "CE:BA:BE:97:DB:0C";
+    private final String uuidRed = "DD:3F:50:F2:76:74";
+
+    BluetoothData bldata;
+
+
+    public int getRssiQueueBlue() {
+        return rssiQueueBlue.size();
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "Im onCreate von BluetoothData");
+        super.onCreate();
+        beaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+
+        beaconManager.bind(this);
+    }
+
+    public BluetoothData(Activity activity){
+        Log.i(TAG, "Im Konstruktor von BluetoothData");
+    }
+
+    public void unbindManager(Activity activity) {
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+
+                /*BluetoothMeasurements blueBeacon = new BluetoothMeasurements(
+                        "blue",uuidBlue,bluePlaces);
+                BluetoothMeasurements yellowBeacon = new BluetoothMeasurements(
+                        "yellow",uuidYellow,yellowPlaces);
+                BluetoothMeasurements redBeacon = new BluetoothMeasurements(
+                        "red",uuidRed,redPlaces);*/
+
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "<---------------------------------------------->");
+                    Log.i(TAG, "beacons>0");
+                    /*Create queues containing the latest 20 values*/
+                    /*while (beacons.iterator().hasNext()) {*/
+
+                    avgRssi =
+                            bldata.queueAssignment(beacons.iterator().next().getBluetoothAddress(),
+                                    beacons.iterator().next().getRssi());
+                    Log.i(TAG, "Die Größe der Queue Blue ist" + bldata.getRssiQueueBlue());
+
+                    Log.i(TAG, avgRssi.toString());
+                }
+            }
+
+        });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+            Log.e(TAG, e.toString());
+        }
     }
 
     public Map<String, Number> queueAssignment(String uuid, int rssi) {
@@ -81,5 +178,31 @@ public class BluetoothData {
             avgSum += element;
         }
         return avgSum / queue.size();
+    }
+
+
+
+        @Override
+    public Context getApplicationContext() {
+        return null;
+    }
+
+    @Override
+    public void unbindService(ServiceConnection serviceConnection) {
+
+    }
+
+    @Override
+    public boolean bindService(Intent intent, ServiceConnection serviceConnection, int i) {
+        return false;
+    }
+
+    public String getRssiOutput() {
+        return "getRssiOutput Output";
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
--- mobile/src/main/java/smartwatch/context/project/activities/QrcodeActivity.java
+++ mobile/src/main/java/smartwatch/context/project/activities/QrcodeActivity.java
@@ -1,6 +1,7 @@
 package smartwatch.context.project.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
@@ -27,63 +28,39 @@ import smartwatch.context.common.helper.BluetoothData;
 import smartwatch.context.common.helper.BluetoothMeasurements;
 import smartwatch.context.project.R;
 
-public class QrcodeActivity extends Activity implements BeaconConsumer, View.OnClickListener {
+public class QrcodeActivity extends Activity{
     private static final String TAG = "RangingActivity";
-    private final Queue<Integer> rssiQueueBlue = new LinkedList<>();
-    private final Queue<Integer> rssiQueueYellow = new LinkedList<>();
-    private final Queue<Integer> rssiQueueRed = new LinkedList<>();
-    private final int queueSize = 5;
-    private final List<Integer> calibrationList = new LinkedList<>();
+
     /*Blue, Yelloow. Red*/
-    private final double[] distances = new double[3];
-    private final String uuidYellow = "FB:39:E6:2D:82:EF";
-    private final String uuidBlue = "CE:BA:BE:97:DB:0C";
-    private final String uuidRed = "DD:3F:50:F2:76:74";
-    //*^+ oder
-    private final double constMult = 0.0001060777;
-    private final double constPower = 17.4228892910;
-    private final double constPlus = 0.7610257596;
-    private BeaconManager beaconManager;
-    private int txPowerBlue;
-    private int txPowerYellow;
-    private int txPowerRed;
+
     private TextView distanceOutput;
     private TextView calibrationOutput;
-
     BluetoothData bldata;
-    Map<String, Number> avgRssi;
 
-    String[] bluePlaces = {"1", "2", "3"};
-    String[] yellowPlaces = {"11", "12", "13"};
-    String[] redPlaces = {"21", "22", "23"};
-
+    /*BluetoothData bldata;*/
+    /*Map<String, Number> avgRssi;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
 
-        Button calculateAverage = (Button) findViewById(R.id.calculate_average);
-        calculateAverage.setOnClickListener(this);
-
-        Button deleteList = (Button) findViewById(R.id.delete_list);
-        deleteList.setOnClickListener(this);
-
         calibrationOutput = (TextView) findViewById(R.id.calibration_average);
         distanceOutput = (TextView) findViewById(R.id.ble_rssi);
 
-        bldata = new BluetoothData(rssiQueueBlue, rssiQueueYellow,rssiQueueRed);
+        startService(new Intent(this, BluetoothData.class));
+        /*bldata = new BluetoothData(this);*/
+        /*distanceOutput.setText(bldata.getRssiOutput());*/
+    }
 
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        beaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
-
-        /*Beacon.setDistanceCalculator(curveDistanceCalculator);*/
-        beaconManager.bind(this);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        /*bldata.unbindManager(this);*/
 
     }
 
-    public void onClick(View view) {
+    /*public void onClick(View view) {
         if (view.getId() == R.id.calculate_average) {
             if (calibrationList.size() >= 20) {
                 double calibrationAverage = BleHelper.calculateAverage(calibrationList);
@@ -98,59 +75,8 @@ public class QrcodeActivity extends Activity implements BeaconConsumer, View.OnC
         if (view.getId() == R.id.delete_list) {
             calibrationList.clear();
         }
-    }
+    }*/
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        beaconManager.unbind(this);
-    }
 
-    @Override
-    public void onBeaconServiceConnect() {
-        beaconManager.setRangeNotifier(new RangeNotifier() {
-            @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-
-                /*BluetoothMeasurements blueBeacon = new BluetoothMeasurements(
-                        "blue",uuidBlue,bluePlaces);
-                BluetoothMeasurements yellowBeacon = new BluetoothMeasurements(
-                        "yellow",uuidYellow,yellowPlaces);
-                BluetoothMeasurements redBeacon = new BluetoothMeasurements(
-                        "red",uuidRed,redPlaces);*/
-
-                if (beacons.size() > 0) {
-                    Log.i(TAG, "<---------------------------------------------->");
-                    Log.i(TAG, "beacons>0");
-                    /*Create queues containing the latest 20 values*/
-                    /*while (beacons.iterator().hasNext()) {*/
-
-                    avgRssi =
-                    bldata.queueAssignment(beacons.iterator().next().getBluetoothAddress(),
-                            beacons.iterator().next().getRssi());
-
-                    Log.i(TAG, avgRssi.toString());
-                }
-
-                /*Necessary to change UI data when no in the main thread*/
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        distanceOutput.setText("Blau: " + distances[0] + "\n" +
-                                "Gelb: " + distances[1] + "\n" +
-                                "Rot: " + distances[2] + "\n" +
-                                "Kalibrierungsliste: " + calibrationList.size());
-                    }
-                });
-            }
-
-        });
-
-        try {
-            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-        } catch (RemoteException e) {
-            Log.e(TAG, e.toString());
-        }
-    }
 
 }
